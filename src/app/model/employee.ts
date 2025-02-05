e49af2889cc7a95949aa372c09fa86822e75d47e
iamShreyaK <shreyakdev@gmail.com>
2025-02-05 12:29:44 +0530

    Added Employee Form

---
+++ src/app/model/employee.ts
@@ -0,0 +1,19 @@
+export class EmployeeModel1{
+    empId : number;
+    empName : string;
+    empCity : string;
+    empState : string;
+    empcontactNo : string;
+    empAddress : string; 
+    empPinccode : string;
+
+    constructor(){
+        this.empId = 0;
+        this.empAddress='';
+        this.empCity = '';
+        this.empState = '';
+        this.empcontactNo = '';
+        this.empName = '';
+        this.empPinccode = '';
+    }
+}
